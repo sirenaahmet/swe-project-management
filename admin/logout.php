afcf38f0bcf9839857fd89ce5c4d3775f94a4c62
gerisonh <gerisonhoxha@gmail.com>
2025-04-10 11:30:28 +0200

    added authentication for admin

---
+++ admin/logout.php
@@ -0,0 +1,28 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the admin is logged in, if not then redirect to login page
+if(!isset($_SESSION["admin_loggedin"]) || $_SESSION["admin_loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destroy the session.
+session_destroy();
+
+// Redirect to login page
+header("location: login.php");
+exit;
